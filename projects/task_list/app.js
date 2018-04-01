428d51fa75c8bcd11966ce5b45c154c5fe92ca52
david-dacruz <dvddcrz@gmail.com>
2018-04-02 00:59:37 +0200

    creating ui variables an add item

---
+++ projects/task_list/app.js
@@ -0,0 +1,48 @@
+// define Ui vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// load all event listeners
+loadEventListeners();
+
+// load event listener
+function loadEventListeners(){
+  // add task event
+  form.addEventListener('submit', addTask);
+}
+
+// add task
+function addTask(e){
+  
+  if(taskInput.value === ''){
+    alert('Add a task');
+  } else {
+    createLiElement();
+  }
+ 
+  e.preventDefault();
+}
+
+// create li element
+function createLiElement(){
+  const li = document.createElement('li');
+  // add class
+  li.className = 'collection-item';
+  // create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // create new link element
+  const link = document.createElement('a');
+  // add class
+  link.className = 'delete-item secondary-content';
+  // add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // append the link to li
+  li.appendChild(link);
+  // append li to ul
+  taskList.appendChild(li);
+  // clear input
+  taskInput.value = '';
+}
