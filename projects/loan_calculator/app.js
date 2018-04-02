24c07742b6237ce91ccbcb3ea009ba2b8aea4917
david-dacruz <dvddcrz@gmail.com>
2018-04-02 23:45:58 +0200

    loader and user experience

--- projects/loan_calculator/app.js
+++ projects/loan_calculator/app.js
@@ -1,5 +1,15 @@
 // listen for submit
-document.querySelector('#loan-form').addEventListener('submit', calculateResults);
+document.querySelector('#loan-form').addEventListener('submit', function(e){
+
+  // hide results 
+  document.getElementById('results').style.display = 'none';
+  // show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+ 
+  e.preventDefault();
+});
 
 // calculate results
 function calculateResults(e){
@@ -23,14 +33,24 @@ function calculateResults(e){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    
+     // show results 
+  document.getElementById('results').style.display = 'block';
+  // hide loader
+  document.getElementById('loading').style.display = 'none';
+
+ 
   } else {
     showError('Please check your numbers');
   }
-  e.preventDefault();
 }
 
 // showError
 function showError(error){
+      // hide results 
+      document.getElementById('results').style.display = 'none';
+      // hide loader
+      document.getElementById('loading').style.display = 'none';
   // create a div
   const errorDiv = document.createElement('div');
   // get elements
