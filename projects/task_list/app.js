0d10c5c889f0d6db81dbd7ef90c98ccdaa390b18
david-dacruz <dvddcrz@gmail.com>
2018-04-02 21:53:43 +0200

    delete and filter items

--- projects/task_list/app.js
+++ projects/task_list/app.js
@@ -12,11 +12,49 @@ loadEventListeners();
 function loadEventListeners(){
   // add task event
   form.addEventListener('submit', addTask);
+  // remove task event
+  taskList.addEventListener('click', removeTask);
+  // clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // filter tasks event
+  filter.addEventListener('keyup', filterTasks);
+}
+
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach
+  (function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
+}
+
+function clearTasks(){
+  // can do taskList.innerHTML = '';
+  // faster
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+//remove task
+function removeTask(e){
+  // to find the target element we need
+  //console.log(e.target);
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure ?')){
+      e.target.parentElement.parentElement.remove();
+    }
+  }
 }
 
 // add task
 function addTask(e){
-  
+  // check that the input isn't empty
   if(taskInput.value === ''){
     alert('Add a task');
   } else {
